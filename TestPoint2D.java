927c4d1a57eeaad112325917c05b210a74b79eff
huynhvantu <huynhvantunlp@gmail.com>
2019-07-01 04:54:03 +0700

    first commit

---
+++ TestPoint2D.java
@@ -0,0 +1,15 @@
+
+public class TestPoint2D {
+
+	public static void main(String[] args) {
+		Point2D point2D = new Point2D();
+		System.out.println(point2D);
+		
+		point2D = new Point2D(1.0f, 2.0f);
+		System.out.println(point2D);
+		
+		
+	}
+	
+
+}
